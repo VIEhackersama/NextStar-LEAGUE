4a5f45aeb720ccd9d95914f3209d094b5ddc7eb5
huyle-uii <huyle4549@gmail.com>
2025-08-25 20:58:46 +0700

    3

--- myapp/src/router/router.js
+++ myapp/src/router/router.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AnimatePresence } from "framer-motion";
+import Homepage from "../pages/homepage";
 const Routers = () => {
   const location = useLocation(); 
   return (
     <AnimatePresence mode="wait">
     <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Homepage />} />
     </Routes>
     </AnimatePresence>
   );
