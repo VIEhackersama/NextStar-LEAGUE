8e332cd31486ec97f5d5698550603ed8023b60ae
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-09-17 14:12:58 +0700

    Update Ha (#51)

--- myapp/src/router/router.js
+++ myapp/src/router/router.js
@@ -5,9 +5,9 @@ import { AnimatePresence } from "framer-motion";
 import Homepage from "../pages/homepage";
 import TeamDetail from "../pages/TeamDetail";
 import Header from "../components/header";
-import HistoryPage from "../pages/HistoryPage";   
-import ClubsPage from "../pages/ClubsPage";     
-import StarsPage from "../pages/StarsPage"; 
+import HistoryPage from "../pages/HistoryPage";
+import ClubsPage from "../pages/ClubsPage";
+import StarsPage from "../pages/StarsPage";
 import NewsPage from '../pages/newPage';
 import NewsDetailPage from '../pages/NewsDetailPage';
 import LoginPage from "../pages/LoginPage";
@@ -17,25 +17,25 @@ import ContactPage from "../pages/ContactPage";
 
 
 const Routers = () => {
-const location = useLocation(); 
-return (
-    <AnimatePresence mode="wait">
-    <Routes location={location} key={location.pathname}>
-    <Route path="/" element={<Navigate to="/home" replace />} />
-    <Route path="/home" element={<Homepage />} />
-    <Route path="/team/:teamId" element={<TeamDetail />} /> 
-    <Route path="/history" element={<HistoryPage />} />
-    <Route path="/clubs" element={<ClubsPage />} />
-    <Route path="/stars" element={<StarsPage />} />
-    <Route path="/news" element={<NewsPage/>} />
-    <Route path="/news/:id" element={<NewsDetailPage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-    <Route path="/contact" element={<ContactPage />} />
-    </Routes>
-    </AnimatePresence>
-);
+    const location = useLocation();
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                <Route path="/home" element={<Homepage />} />
+                <Route path="/team/:teamId" element={<TeamDetail />} />
+                <Route path="/history" element={<HistoryPage />} />
+                <Route path="/clubs" element={<ClubsPage />} />
+                <Route path="/stars" element={<StarsPage />} />
+                <Route path="/news" element={<NewsPage />} />
+                <Route path="/news/:id" element={<NewsDetailPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+        </AnimatePresence>
+    );
 };
 
 export default Routers;
